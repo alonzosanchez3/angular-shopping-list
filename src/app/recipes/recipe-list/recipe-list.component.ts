aaf37d09b343518c8bc6a9153d5260c1f885c86f
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-04-30 22:27:06 -0500

    added shopping list skeleton

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is a test', 'https://www.savorynothings.com/wp-content/uploads/2022/01/oven-baked-salmon-recipe-image-9.jpg')
+  ];
+
+
+}
